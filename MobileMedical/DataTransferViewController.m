9de5a4f92a771623542de0ae5deeaeb8ec4e0869
liyuanchao <lych0317@126.com>
2014-10-27 19:07:07 +0800

    add scan peripherals feature

--- MobileMedical/DataTransferViewController.m
+++ MobileMedical/DataTransferViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "DataTransferViewController.h"
+#import <LGBluetooth/LGBluetooth.h>
+#import "Utils.h"
 
 @interface DataTransferViewController ()
 
@@ -16,22 +18,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [[LGCentralManager sharedInstance] scanForPeripheralsByInterval:10 completion:^(NSArray *peripherals) {
+        [peripherals enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            LGPeripheral *peripheral = obj;
+            NSLog(@"peripheral name is %@, uuid is %@", peripheral.name, peripheral.UUIDString);
+        }];
+    }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
